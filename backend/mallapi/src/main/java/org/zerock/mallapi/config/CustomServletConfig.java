fecbf20a6b9c4a58b9a681e6dd782ea17d0cdcc4
miyeon Kim <aldus9714@naver.com>
2025-07-11 14:45:37 +0900

    test2

--- backend/mallapi/src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
+++ backend/mallapi/src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
@@ -5,7 +5,7 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.zerock.mallapi.controller.formatter.LocalDateFormatter;
-
+//
 @Configuration
 public class CustomServletConfig implements WebMvcConfigurer{
 
